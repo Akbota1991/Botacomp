65d1a02b0efd88c5afa0b57588a1f4cefae3c600
akbota <a.nasvetova91@gmail.com>
2022-12-22 18:46:36 -0700

    test: qa-123 add testing of login functionality

---
+++ src/akbota/TestingLoginFunctionality.java
@@ -0,0 +1,8 @@
+package akbota;
+
+public class TestingLoginFunctionality {
+    public static void main(String[] args) {
+        System.out.println("I am testing Login functionality of my company");
+
+    }
+}
